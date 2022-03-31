636b3f999064badf2f7fcf04a1d75b77b2c430dd
Alistair Duncan <a.duncan@it-frankfurt.com>
2022-03-31 10:45:04 +0200

    karriere seite fertig + cms angepasst

--- themes/meghna-hugo/assets/js/script.js
+++ themes/meghna-hugo/assets/js/script.js
@@ -272,5 +272,9 @@ $(document).ready(function(){
 
 });
 
-
-
+// Accordions
+$('.collapse').on('shown.bs.collapse', function () {
+	$(this).parent().find('.ti-plus').removeClass('ti-plus').addClass('ti-minus');
+}).on('hidden.bs.collapse', function () {
+	$(this).parent().find('.ti-minus').removeClass('ti-minus').addClass('ti-plus');
+});
